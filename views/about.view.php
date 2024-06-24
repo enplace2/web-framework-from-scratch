c785d388bdc06d39d2be9ca134c481a787d759c8
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-06-23 20:56:28 -0400

    Update about.view.php

--- views/about.view.php
+++ views/about.view.php
@@ -7,6 +7,7 @@
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             Whats the word lemme tell ya
         </div>
+    </main>
 </div>
 
 <?php require "partials/footer.php" ?>
