0a9d710b3559807ad8fca3cc53492e3f7f063243
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-08-11 14:01:49 -0400

    Update response.php

--- globals/response.php
+++ globals/response.php
@@ -2,6 +2,13 @@
 
 use Core\Response\Response;
 
+/**
+ * A global function for interacting with the Response class
+ * @param $content
+ * @param int $status
+ * @param array $headers
+ * @return Response
+ */
 function response($content = '', int $status = 200, array $headers = []): Response
 {
     return new Response($content, $status, $headers);
