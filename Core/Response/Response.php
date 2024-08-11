fda4f2bc8155fb079bfc8bc8b805c83b83d9cc45
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-08-11 14:01:13 -0400

    add comments

--- Core/Response/Response.php
+++ Core/Response/Response.php
@@ -3,7 +3,7 @@
 namespace Core\Response;
 class Response
 {
-    protected mixed $content;
+    protected $content;
     protected int $status;
     protected array $headers = [];
 
@@ -14,12 +14,22 @@ class Response
         $this->headers = $headers;
     }
 
+    /**
+     * Sets the header value on the Response instance for a given key value pair
+     * @param string $key
+     * @param string $value
+     * @return $this
+     */
     public function header(string $key, string $value): Response
     {
         $this->headers[$key] = $value;
         return $this;
     }
 
+    /**
+     * Handles sending the response by setting the status code, headers, and echoing the content
+     * @return void
+     */
     public function send(): void
     {
         http_response_code($this->status);
@@ -32,7 +42,15 @@ class Response
         exit;
     }
 
-    public static function json(mixed $data, int $status = 200, array $headers = []): Response
+    /**
+     * Json encodes content and sets the content type header to 'application/json'
+     *
+     * @param $data
+     * @param int $status
+     * @param array $headers
+     * @return Response
+     */
+    public static function json($data, int $status = 200, array $headers = []): Response
     {
         $instance = new self(json_encode($data), $status, $headers);
         $instance->header('Content-Type', 'application/json');
