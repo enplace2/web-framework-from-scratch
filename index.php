c22d4b6c43b8987beabeaed6489e3a44e536cf7f
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-08-12 20:47:07 -0400

    use kernel to handle request

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 require "globals/globals.php";
 require "autoload.php";
-use Core\Router\Router;
 require "routes/routes.php";
 
+use Core\Kernel\Kernel;
 
-$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-$method = $_SERVER["REQUEST_METHOD"];
-Router::resolve($uri, $method);
+$kernel = new Kernel();
+$kernel->handle()->send();
