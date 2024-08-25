25edd80a4ea111bcd54a3603a40b28b4b76f0502
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-08-25 16:54:57 -0400

    allow for singletons in container

--- Core/Container/Container.php
+++ Core/Container/Container.php
@@ -8,10 +8,18 @@ class Container
 {
     protected array $bindings = [];
     protected array $singletons = [];
-    public function bind(string $abstract, string|Closure|null $concrete = null): void
+    public function bind(string $abstract, string|Closure|null $concrete = null, $singleton = false): void
     {
         if(!$concrete) $concrete = $abstract;
-        $this->bindings[$abstract] = $concrete;
+        $this->bindings[$abstract] = [
+            "concrete"  => $concrete,
+            "singleton" => $singleton,
+        ];
+    }
+
+    public function singleton(string $abstract, string|Closure|null $concrete = null): void
+    {
+        $this->bind($abstract, $concrete, true);
     }
 
     public function resolve(string $abstract)
@@ -20,14 +28,25 @@ class Container
         {
             return $this->singletons[$abstract];
         }
-        else return $this->build($this->bindings[$abstract]);
+        else return $this->build($abstract);
     }
 
-    public function build($concrete)
+    protected function build(string $abstract)
+    {
+        $bindings = $this->bindings[$abstract];
+
+        if($bindings["singleton"] === true){
+            $this->singletons[$abstract] = $this->instantiate($abstract, $bindings["concrete"]);
+            return $this->singletons[$abstract];
+        }
+        return $this->instantiate($abstract, $bindings["concrete"]);
+    }
+
+    protected function instantiate($abstract, $concrete)
     {
         if(is_callable($concrete)){
             return $concrete();
         }
-
+        throw new \Exception("Instantiation failed. Closure provided for $abstract is not callable");
     }
 }
