81ee5023a583ee5eaa9b0aad3584754148706b41
Nick Place <114956247+enplace2@users.noreply.github.com>
2024-09-21 19:53:27 -0300

    handle action resolution in Route class

--- Core/Router/Route/Route.php
+++ Core/Router/Route/Route.php
@@ -2,23 +2,27 @@
 
 namespace Core\Router\Route;
 
-use Core\Middleware\MiddlewareInterface;
+use Closure;
+use Core\Pipeline\Pipe\PipeInterface;
 
 class Route
 {
+    public array $middleware = [];
+    public array $excludedMiddleware = [];
+    protected array $params = [];
+
     public function __construct(
         public string $method,
         public string $uri,
         public array  $action,
-        public array  $middleware = [],
-        public array $excludedMiddleware =[]
     )
     {
     }
+
     /**
      * Add middleware to the route.
      *
-     * @param array<string<MiddlewareInterface>> $middlewareClasses
+     * @param array<string<PipeInterface>> $middlewareClasses
      * @return static
      */
     public function middleware(array $middlewareClasses): static
@@ -32,7 +36,7 @@ class Route
     /**
      * Add middleware to be excluded from the route.
      *
-     * @param array<string<MiddlewareInterface>> $middlewareClasses
+     * @param array<string<PipeInterface>> $middlewareClasses
      * @return static
      */
     public function withoutMiddleware(array $middlewareClasses): static
@@ -41,5 +45,33 @@ class Route
             $this->excludedMiddleware[] = $middlewareClass;
         }
         return $this;
+    }
+
+    public function setParams(array $params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * Returns the route action with the proided params
+     * @return Closure
+     * @throws \Exception
+     */
+    public function getAction(): Closure
+    {
+        list ($class, $method) = $this->action;
+        //Todo: should create specific exception classes for these
+        if (!class_exists($class)) {
+            throw new \Exception("Class $class not found");
+        }
+
+        if (!method_exists($class, $method)) {
+            throw new \Exception("Method $method not found on class $class");
+        }
+
+        return function () use ($class, $method){
+            return (new $class())->$method(...$this->params);
+    };
     }
 }
